8db77cb298c3d1aeb4b3d8b37c8145e1c641917b
hiepptit99 <44068449+hiepptit99@users.noreply.github.com>
2018-10-15 09:40:45 +0700

    Create spark.sh

---
+++ spark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+sudo apt-get update
+
+sudo add-apt-repository ppa:webupd8team/java
+
+sudo apt-get update 
+
+sudo apt-get install -y oracle-java8-installer
+
+sudo apt-get install -y python-pip
+
+sudo pip install pyspark
